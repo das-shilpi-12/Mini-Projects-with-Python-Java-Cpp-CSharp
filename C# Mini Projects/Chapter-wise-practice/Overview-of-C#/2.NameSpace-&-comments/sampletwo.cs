4de15669e76b5690388feb3eb9896eb2eaa2db02
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-03 09:33:44 +0530

    Create sampletwo.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Overview-of-C#/2.NameSpace-&-comments/sampletwo.cs
@@ -0,0 +1,7 @@
+//Namespace & Comments
+using System; // System is a Namespace
+class sampletwo{
+    public static void Main(){
+        Console.WriteLine("No need to use System in Output line"); //output line
+    }
+}
