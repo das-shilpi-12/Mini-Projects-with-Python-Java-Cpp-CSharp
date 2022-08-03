4cfa1e5d080c1fbe12126f7154dfc1fa54f13b05
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-03 10:55:17 +0530

    Create samplesix.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Overview-of-C#/6.Argument-passing/samplesix.cs
@@ -0,0 +1,11 @@
+//Command Line Arguments
+using System;
+class samplesix{
+    public static void Main(string[] args){
+       double a = 9.0;//value of argument
+       double s;
+       s = Math.Sqrt(a);
+       Console.WriteLine(s);
+
+    }
+}
