86677a22ff09794e8e6bcc6b88b92ba067e293a0
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-03 09:45:13 +0530

    Create samplefour.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Overview-of-C#/4.Using-alias/samplefour.cs
@@ -0,0 +1,7 @@
+//Using alias for namespace class
+using A = System.Console; // A is alias of System.Console
+class samplefour{
+    public static void Main(){
+        A.WriteLine("Creating Alias A for System.Console");
+    }
+}
