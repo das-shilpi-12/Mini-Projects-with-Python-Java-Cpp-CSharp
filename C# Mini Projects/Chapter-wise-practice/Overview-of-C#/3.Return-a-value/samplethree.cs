b70a234b5133cf570c0a1c49a45c22e15616e879
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-03 09:38:41 +0530

    Create samplethree.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Overview-of-C#/3.Return-a-value/samplethree.cs
@@ -0,0 +1,8 @@
+//main will return a integer type value
+using System;
+class samplethree{
+    public static int Main(){
+        Console.WriteLine("It will return Integer");
+        return 0; // Return statement
+    }
+}
