c08d4f779b0b82995dd5805c997198c192939dbe
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-04 08:24:46 +0530

    Create Example.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Literals-Variables-Datatypes/Literals/Example.cs
@@ -0,0 +1,10 @@
+using System;
+class example{
+ public static void Main(){
+     Console.WriteLine("Integer Literal : {0}",24);
+     Console.WriteLine("Double Literal : {0}",23.456356743);
+     Console.WriteLine("Decimal Literal : {0}",1.789768767);
+     Console.WriteLine("Exponential notation for Double  Literal :{0}",5.8765E6f);
+     Console.WriteLine("Hexadecimal Literal :{0}",0x003A);
+ }   
+}
