8b8180dda360fea778ac7a1c0997c766339f8c52
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-04 08:16:40 +0530

    Create example.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Literals-Variables-Datatypes/Identifiers/example.cs
@@ -0,0 +1,9 @@
+using System;
+class example{
+    public static void Main(){
+        int @a;//declearing a as identifier.
+        for (@a = 0;@a<10;@a++){
+        Console.WriteLine("The value of @a is :{0}",@a);
+        }
+    }
+}
