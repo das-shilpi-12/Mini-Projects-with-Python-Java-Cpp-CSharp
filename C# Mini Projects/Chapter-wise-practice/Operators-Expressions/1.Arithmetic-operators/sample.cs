cbce8f3163059a50e000bf15fb7e10293e453317
Shilpi Das <90592521+das-shilpi-12@users.noreply.github.com>
2022-08-04 10:13:01 +0530

    Create sample.cs

---
+++ C# Mini Projects/Chapter-wise-practice/Operators-Expressions/1.Arithmetic-operators/sample.cs
@@ -0,0 +1,15 @@
+//floating point arithmetic
+using System;
+class sample{
+    public static void Main(){
+        float a = 20.5F;
+        float b = 6.4F;
+        Console.WriteLine("a = "+a);
+        Console.WriteLine("b = "+b);
+        Console.WriteLine("a+b = "+(a+b));
+        Console.WriteLine("a-b = "+(a-b));
+        Console.WriteLine("a*b = "+(a*b));
+        Console.WriteLine("a/b = "+(a/b));
+        Console.WriteLine("a%b = "+(a%b));
+    }
+}
